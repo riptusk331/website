54d309018adc98394f55892ab8ac66f5370259cc
Ryan Joyce <ryan.joyce.88@gmail.com>
2022-12-07 17:32:07 -0500

    get quick demo site working

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
+from datetime import date, datetime
 from flask import Flask, jsonify, request
+from simplejson import JSONEncoder
+from ua_parser import user_agent_parser
+
+
+class MyJSONEncoder(JSONEncoder):
+    """Subclass of simplejson's encoder to override the default
+    behavior of serialzing datetimes to RFC1123 standard
+    """
+
+    def default(self, obj):
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat()
+        try:
+            encoder = super().default(obj)
+        except TypeError:
+            encoder = obj.__repr__()
+        finally:
+            return encoder
+
 
 app = Flask(__name__)
+app.json_encoder = MyJSONEncoder
+
 
 @app.route("/")
 def home():
-    return jsonify(dict(request.headers))
+    user_agent = user_agent_parser.Parse(request.headers["User-Agent"])
+    return (
+        f"Welcome to Ryan's website. It looks like you're visiting me from {request.remote_addr} on a "
+        f"{user_agent['os']['family']} {user_agent['os']['major']} machine running "
+        f"{user_agent['user_agent']['family']} {user_agent['user_agent']['major']}.{user_agent['user_agent']['minor']}"
+    )
